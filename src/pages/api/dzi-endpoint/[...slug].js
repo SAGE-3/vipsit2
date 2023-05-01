ae83f98bb8fea5e8ecb132593416d8f182f8d518
renambot <renambot@gmail.com>
2023-05-01 15:08:06 -0500

    fix: download xml

--- src/pages/api/dzi-endpoint/[...slug].js
+++ src/pages/api/dzi-endpoint/[...slug].js
@@ -6,6 +6,6 @@ export default function handler(req, res) {
   console.log("dziPath", dziPath);
   const filePath = path.resolve(path.join("public", dziPath));
   const imageBuffer = fs.readFileSync(filePath);
-  res.setHeader("Content-Type", "application/xml");
+  // res.setHeader("Content-Type", "application/xml");
   return res.send(imageBuffer);
 }
